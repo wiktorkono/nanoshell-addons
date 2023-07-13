e6f061d2cfb2760a61d79406acb2994675be0bdd
Mihael <93058030+Mihael3007@users.noreply.github.com>
2023-07-13 11:04:31 +0200

    Fix AddonInstaller

--- builtin/addon_installer.py
+++ builtin/addon_installer.py
@@ -4,34 +4,37 @@ from bin.coloramasetup import *
 addonList = []
 
 def checkForRequest(prompt):
-    if prompt != "install" or prompt != "install ": return prompt.replace("install ", "")
-    else: return ""
+    if prompt.startswith("install"):
+        return prompt.replace("install", "").strip()
+    else:
+        return prompt
+
 
 def downloadRepo():
     print("Clone addons repo")
     dload.git_clone("https://github.com/Kwadratz/nanoshell-addons.git")
     print("Organize folders...")
-    shutil.move("./addons/AddonInstaller/nanoshell-addons/nanoshell-addons-main", "./addons/AddonInstaller/")
-    shutil.rmtree("./addons/AddonInstaller/nanoshell-addons")
-    os.rename("./addons/AddonInstaller/nanoshell-addons-main", "./addons/AddonInstaller/addonsRepo")
+    shutil.move("./addons/builtin/nanoshell-addons/nanoshell-addons-main", "./addons/builtin/")
+    shutil.rmtree("./addons/builtin/nanoshell-addons")
+    os.rename("./addons/builtin/nanoshell-addons-main", "./addons/builtin/addonsRepo")
     print("Remove unecessary files")
-    os.remove("./addons/AddonInstaller/addonsRepo/.gitignore")
-    os.remove("./addons/AddonInstaller/addonsRepo/LICENSE")
-    os.remove("./addons/AddonInstaller/addonsRepo/README.md")
+    os.remove("./addons/builtin/addonsRepo/.gitignore")
+    os.remove("./addons/builtin/addonsRepo/LICENSE")
+    os.remove("./addons/builtin/addonsRepo/README.md")
 
 def fetchAddons():
     global addonList
     print("Retrieve addon list")
-    addonList = os.listdir("./addons/AddonInstaller/addonsRepo")
+    addonList = os.listdir("./addons/builtin/addonsRepo")
     print(addonList)
 
 def cleanUp():
     print("Delete cloned addons repo")
-    shutil.rmtree("./addons/AddonInstaller/addonsRepo")
+    shutil.rmtree("./addons/builtin/addonsRepo")
     print("Installation complete. Please exit Nanoshell and run 'run.py'.")
 
 def installer(pickedAddon):
-    addonRelativePath = os.path.abspath(os.path.join("./addons/AddonInstaller/addonsRepo", pickedAddon))
+    addonRelativePath = os.path.abspath(os.path.join("./addons/builtin/addonsRepo", pickedAddon))
     destinationPath = os.path.abspath(os.path.join("./addons", pickedAddon))
     if os.path.exists(destinationPath):
         print("Destination path already exists. Deleting existing directory.")
@@ -44,33 +47,35 @@ def menu():
     print(f"{r}{c.GREEN}Addon Installer")
     print(f"{r}Listing all addons:\n")
     for addon in addonList:
-        print({dim}addon{r})
+        print(dim + addon + r)
     pickedAddon = input(f"\nEnter the name of the addon you'd like to install {dim}>{r} ")
     if pickedAddon != "":
         if pickedAddon in addonList:
             installAddon(pickedAddon)
         else:
             os.system('cls' if os.name=='nt' else 'clear')
-            shutil.rmtree("./addons/AddonInstaller/addonsRepo")
+            shutil.rmtree("./addons/builtin/addonsRepo")
             input(f"{c.RED}Unknown addon{r}{dim}, press [ENTER] to continue...{r}")
             menu()
 
 def installAddon(request):
     if request in addonList:
-        addonRelativePath = os.path.abspath(os.path.join("./addons/AddonInstaller/addonsRepo", request)) # pick addon relative path
-        destinationPath = os.path.abspath(os.path.join("./addons", request)) # move picked addon to addons/
+        addonRelativePath = os.path.abspath(os.path.join("./addons/builtin/addonsRepo", request))
+        destinationPath = os.path.abspath(os.path.join("./addons", request))
         if os.path.exists(destinationPath):
             print(f"{c.RED}Destination path already exists. Deleting existing directory.{r}")
             shutil.rmtree(destinationPath)
         shutil.copytree(addonRelativePath, destinationPath)
         cleanUp()
-        shutil.rmtree("./addons/AddonInstaller/addonsRepo") # remove tree addonsrepo
         print(f"{r}{c.GREEN}Successfully installed {request}")
         print(f"{r}{dim}To apply changes, restart or reload your Nanoshell instance ('reload' command){r}")
+
 
 def main(prompt):
     addonRequest = checkForRequest(prompt)
     downloadRepo()
     fetchAddons()
-    if addonRequest == "": menu()
-    else: installAddon(addonRequest)
+    if addonRequest == "":
+        menu()
+    else:
+        installAddon(addonRequest)
