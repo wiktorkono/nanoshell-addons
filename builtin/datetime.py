e8b82e730493e4483eddf47b9a80773ff1b81ca4
Mihael <93058030+Mihael3007@users.noreply.github.com>
2023-07-25 13:57:04 +0200

    Update DateTime
    Change main function name
    Better formatting

--- builtin/datetime.py
+++ builtin/datetime.py
@@ -1,5 +1,7 @@
 import datetime
 
-def main(prompt):
+def showDateTime(prompt):
     current_time = datetime.datetime.now()
-    print(current_time)
+    formatted_date = current_time.strftime("%d-%m-%Y")
+    formatted_time = current_time.strftime("%H:%M:%S")
+    print(f"{formatted_date} || {formatted_time}")
