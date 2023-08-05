648c8dc1db74b83b70e97a18b9632a8575fdea73
Mihael <93058030+Mihael3007@users.noreply.github.com>
2023-08-05 10:10:57 +0200

    Add arguments to win95keygen

--- Win95keygen/main.py
+++ Win95keygen/main.py
@@ -1,6 +1,6 @@
 from addons.Win95keygen.functions import cd_keygen, oem_keygen
 
-def keyPicker(prompt):
+def uiPicker():
     key_pick = input("Enter key type (CD/oem): ")
     key_pick = key_pick.lower()
 
@@ -18,3 +18,19 @@ def keyPicker(prompt):
 
     else:
         print("Invalid choice.")
+
+
+def keyPicker(prompt):
+    if prompt.startswith('win95keygen '):
+        keyType = prompt.split(' ', 1)[1]
+        keyType = keyType.lower()
+        if keyType == "cd":
+            key = cd_keygen()
+            print(key)
+        elif keyType == "oem":
+            key = oem_keygen()
+            print(key)
+        else:
+            print("Invalid choice.")
+    else:
+        uiPicker()
