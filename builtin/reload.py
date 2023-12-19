403f251e089f853155542e32e0437c5de53ad73c
Mihael Jajtić <93058030+Mihael3007@users.noreply.github.com>
2023-12-19 17:37:01 +0100

    Fix AddonReload
    Implemented the new platform checking from run.py

--- builtin/reload.py
+++ builtin/reload.py
@@ -1,7 +1,16 @@
 import os, json
 
 def main(prompt):
-    addonList = [] #[0 name, 1 triggerCmd, 2 initFoo, 3 imports]
+    # get system platform
+    system_platform = os.name
+    if system_platform == "nt":
+        system_platform = "win"
+    elif system_platform == "posix":
+        system_platform = "linux"
+    else:
+        system_platform = "unknown"
+
+    addonList = []  # [0 name, 1 triggerCmd, 2 initFoo, 3 imports, 4 platform]
 
     addons = os.listdir("addons")
     howManyAddons = 0
@@ -15,38 +24,50 @@ def main(prompt):
         for obj in addonScripts:
             if obj.endswith(".json"):
                 addonData = json.load(open(os.path.abspath(os.path.join("addons", addon, obj))))
-                addonList.append([addonData["name"], addonData["triggerCmd"], addonData["initFoo"], addonData["imports"]])
+                secondaryName = addonData["name"].replace(" ", "").replace("-", "").replace("_", "")
+                addonList.append([addonData["name"], addonData["triggerCmd"], f"{secondaryName}{addonData['initFoo']}",
+                                  addonData["imports"], addonData["platform"]])
                 howManyScripts += 1
     print(f"Found {howManyAddons} addons ({howManyScripts} scripts total)")
 
     print("Reading the base file...")
-    with open(os.path.join("bin", "nanoshell-base.py"), "r") as f: nanoshellBase = f.read() #reading the base file
+    with open(os.path.join("bin", "nanoshell-base.py"), "r") as f:
+        nanoshellBase = f.read().replace("{platform-placeholder}",
+                                         f'"{system_platform}"')  # reading the base file and replacing platform placeholder
 
     print("Writing the base file...")
-    with open("nanoshell.py", "w") as f: f.write(nanoshellBase) #writing the main file
+    with open("nanoshell.py", "w") as f:
+        f.write(nanoshellBase)  # writing the main file
 
     print("Loading addons (part 1)...")
-    with open("nanoshell.py", "a") as f: # here nanoshell.py is being initialized
+    with open("nanoshell.py", "a") as f:  # here nanoshell.py is being initialized
         i = 0
         for addon in addonList:
-            if i != 0: el = "el" # so it can be elif instead of if
-            else: el = ""
+            if i != 0:
+                el = "el"  # so it can be elif instead of if
+            else:
+                el = ""
             triggerCmd = addon[1]
             initFoo = addon[2]
-            f.write(f"\n    {el}if prompt.startswith('{triggerCmd}'): {initFoo}(prompt)")
+            platform = addon[4]
+
+            f.write(f"\n    {el}if prompt.startswith('{triggerCmd}'): ")
+            if platform.lower() == 'all' or platform.lower() == system_platform.lower():
+                f.write(f"{initFoo}(prompt)")
+            else:
+                f.write(f"platformWarning()")
             i += 1
 
     print("Loading addons (part 2)...")
-    with open("imports.py", "w") as f: # here imports.py is being cleared
+    with open("imports.py", "w") as f:  # here imports.py is being cleared
         f.write("")
 
     print("Loading addons (part 3)...")
-    with open("imports.py", "a") as f: # here imports are written to imports.py
+    with open("imports.py", "a") as f:  # here imports are written to imports.py
         for addon in addonList:
             importLines = addon[3]
             for importLine in importLines:
-                f.write(f"\nfrom {importLine[0]} import {importLine[1]}")
+                f.write(f"\nfrom {importLine[0]} import {importLine[1]} as {addon[2]}")
 
-    print("Finished, reloading Nanoshell...")
+    print("Finished, executing Nanoshell...")
     os.system("python nanoshell.py")
-    quit()
