929a7c49fd245ff919a76e6b0ea99524afd1acdc
jajtic <jajtic.mihael@gmail.com>
2024-07-22 13:04:19 +0200

    Update reload

--- builtin/reload.py
+++ builtin/reload.py
@@ -1,22 +1,31 @@
 import os, json
+import platform as platfrm
 
 def main(prompt):
     # get system platform
-    system_platform = os.name
-    if system_platform == "nt":
+    system_platform = platfrm.system()
+    if system_platform == "Windows":
         system_platform = "win"
-    elif system_platform == "posix":
+    elif system_platform == "Linux":
         system_platform = "linux"
+    elif system_platform == "Darwin":
+        system_platform = "darwin"
     else:
         system_platform = "unknown"
 
-    addonList = []  # [0 name, 1 triggerCmd, 2 initFoo, 3 imports, 4 platform]
+    print("Searching for addons...")
+    addonList = [] #[0 name, 1 triggerCmd, 2 initFoo, 3 imports, 4 platform]
 
     addons = os.listdir("addons")
+    for addon in addons:
+        addonPath = "./addons/" + addon
+        if os.path.isdir(addonPath) == True:
+            pass
+        else:
+            addons.remove(addon)
+            print(f'"{addon}" ({addonPath}) is not a directory')
     howManyAddons = 0
     howManyScripts = 0
-    # print(f"Now listing addons: {addons}")
-    print("Searching for addons...")
     for addon in addons:
         howManyAddons += 1
         addonScripts = os.listdir(os.path.join("addons", addon))
@@ -25,19 +34,18 @@ def main(prompt):
             if obj.endswith(".json"):
                 addonData = json.load(open(os.path.abspath(os.path.join("addons", addon, obj))))
                 secondaryName = addonData["name"].replace(" ", "").replace("-", "").replace("_", "")
-                addonList.append([addonData["name"], addonData["triggerCmd"], f"{secondaryName}{addonData['initFoo']}",
-                                  addonData["imports"], addonData["platform"]])
+                addonList.append([addonData["name"], addonData["triggerCmd"], f"{secondaryName}{addonData['initFoo']}", addonData["imports"], addonData["platform"]])
                 howManyScripts += 1
     print(f"Found {howManyAddons} addons ({howManyScripts} scripts total)")
+    print(f"Now listing addons: {addons}")
 
     print("Reading the base file...")
     with open(os.path.join("bin", "nanoshell-base.py"), "r") as f:
-        nanoshellBase = f.read().replace("{platform-placeholder}",
-                                         f'"{system_platform}"')  # reading the base file and replacing platform placeholder
+        nanoshellBase = f.read().replace("{platform-placeholder}", f'"{system_platform}"') #reading the base file and replacing platform placeholder
+
 
     print("Writing the base file...")
-    with open("nanoshell.py", "w") as f:
-        f.write(nanoshellBase)  # writing the main file
+    with open("nanoshell.py", "w") as f: f.write(nanoshellBase) #writing the main file
 
     print("Applying configs...")
     with open("config/preferences.json", "r") as f:
@@ -48,7 +56,7 @@ def main(prompt):
     with open("bin/coloramasetup.py", "a") as f: f.write(f"\na = {a}")
 
     print("Loading addons (part 1)...")
-    with open("nanoshell.py", "a") as f:  # here nanoshell.py is being initialized
+    with open("nanoshell.py", "a") as f: # here nanoshell.py is being initialized
         i = 0
         for addon in addonList:
             if i != 0:
@@ -65,19 +73,20 @@ def main(prompt):
             else:
                 f.write(f"platformWarning()")
             i += 1
+        f.write("\n    else: print(f'{bright}{red}Error:{r} Command not found. Type {a}{bright}help{r} for the list of all commands')")
 
     print("Loading addons (part 2)...")
-    with open("imports.py", "w") as f:  # here imports.py is being cleared
+    with open("imports.py", "w") as f: # here imports.py is being cleared
         f.write("")
 
     print("Loading addons (part 3)...")
-    with open("imports.py", "a") as f:  # here imports are written to imports.py
+    with open("imports.py", "a") as f: # here imports are written to imports.py
         for addon in addonList:
             importLines = addon[3]
             for importLine in importLines:
                 f.write(f"\nfrom {importLine[0]} import {importLine[1]} as {addon[2]}")
 
-    print("Finished, executing Nanoshell...")
+    print("Finished, starting Nanoshell...")
     if system_platform == "win": os.system("python nanoshell.py")
     else: os.system("python3 nanoshell.py")
     exit()
