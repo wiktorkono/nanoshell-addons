39a22c8dff3600c4d1caff3852c83096028d1938
Mihael3007 <jajtic.mihael@gmail.com>
2024-08-30 20:52:42 +0200

    Add colors to nsLibViewer (1.0.1)

--- nsLibViewer/nsLibViewer.py
+++ nsLibViewer/nsLibViewer.py
@@ -1,22 +1,22 @@
 import sys
-from bin.coloramasetup import a as accentColor, r as resetColor
+from bin.coloramasetup import a as accentColor, r as resetColor, red as errorColor
 
 def nsLibViewer(prompt):
     try:
         if not prompt.startswith("nslib "):
-            print("Error: Missing argument. Use 'nslib <variable_name1> [variable_name2] ...'")
+            print(f"{errorColor}Error:{resetColor} Missing argument. Use 'nslib <variable_name1> [variable_name2] ...'")
             return
 
         variable_names = prompt.split(" ")[1:]
 
         if not variable_names:
-            print("Missing argument: no variable specified")
+            print(f"{errorColor}Missing argument:{resetColor} no variable specified")
             return
 
         nanoshell_lib = sys.modules.get('bin.nanoshell_lib')
 
         if not nanoshell_lib:
-            print("Error: bin.nanoshell_lib module not found")
+            print(f"{errorColor}Error:{resetColor} bin.nanoshell_lib module not found")
             return
 
         for requestedVariable in variable_names:
@@ -24,7 +24,7 @@ def nsLibViewer(prompt):
                 variable_value = getattr(nanoshell_lib, requestedVariable)
                 print(f"{accentColor}{requestedVariable}{resetColor} = {variable_value}")
             else:
-                print(f"Error: variable '{requestedVariable}' not found in bin.nanoshell_lib")
+                print(f"{errorColor}Error:{resetColor} variable {errorColor}'{requestedVariable}'{resetColor} not found in bin.nanoshell_lib")
 
     except IndexError:
-        print("Missing argument: no variable specified")
+        print(f"{errorColor}Missing argument:{resetColor} no variable specified")
